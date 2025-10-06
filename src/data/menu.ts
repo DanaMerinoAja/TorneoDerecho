d343dae08cc6c12846dd10e8e4dad75ebf074b26
Dana <dsmerinoh@gmail.com>
2025-10-05 22:38:33 -0500

    header

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -3,14 +3,7 @@
 export const headerMenu = [
     { name: 'Inicio', link: '/#inicio' },
     { name: 'Nosotros', link: '/#nosotros' },
-    { name: 'Servicios', link: '/#servicios', showArrow: true,
-        children: [
-            { name: 'Mantenimiento mecanico y eléctrico', link: '/servicios/mantenimiento-mecanico-electrico/' },
-            { name: 'Mantenimiento a sistemas neumáticos e hidráulicos', link: '/servicios/mantenimiento-neumaticos/' },
-            { name: 'Maestranza', link: '/servicios/maestranza/' },
-            { name: 'Fabricación de estructuras metálicas', link: '/servicios/fabricacion-estructuras-metalicas/' },
-            { name: 'Accesorios industriales y formatería corporativa', link: '/servicios/venta-accesorios/' }
-        ] }
+    { name: 'Beneficios', link: '/#servicios', showArrow: true}
 ];
 
 
